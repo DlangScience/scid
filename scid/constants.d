5d9471ad9b2e87c55d66dd846fff500960b9c370
Lars T. Kyllingstad <git@kyllingen.net>
2010-09-17 14:32:04 +0200

    Added scid.constants, a module for mathematical and physical constants

---
+++ scid/constants.d
@@ -0,0 +1,13 @@
+/** Various mathematical and physical constants.
+
+    Authors:    Lars Tandle Kyllingstad
+    Copyright:  Copyright (c) 2010, Lars T. Kyllingstad. All rights reserved.
+    License:    Boost License 1.0
+*/
+module scid.constants;
+
+
+
+/// Euler-Mascheroni constant, &gamma;.
+enum real eulerGamma = 0.57721_56649_01532_86060L;
+
