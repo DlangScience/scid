ebada6a56aa29a103a007832e3ed3454a6141839
Lars T. Kyllingstad <git@kyllingen.net>
2010-09-17 14:54:21 +0200

    Added Planck constant

--- scid/constants.d
+++ scid/constants.d
@@ -1,4 +1,4 @@
-/** Various mathematical and physical constants.
+/** Various fundamental constants.
 
     Authors:    Lars Tandle Kyllingstad
     Copyright:  Copyright (c) 2010, Lars T. Kyllingstad. All rights reserved.
@@ -8,6 +8,26 @@ module scid.constants;
 
 
 
-/// Euler-Mascheroni constant, &gamma;.
+
+// ==================== PHYSICAL CONSTANTS ====================
+
+/// Planck constant, &#x210E; (J s)
+enum real planckConstant = 6.62606_896e-34L;
+
+/// Planck constant, &#x210E; (eV s)
+enum real planckConstant_eVs = 4.13566_733e-15L;
+
+/// Planck constant divided by 2 pi, &#x210F; (J s)
+enum real hBar = 1.05457_1628e-34L;
+
+/// Planck constant divided by 2 pi, &#x210F; (eV s)
+enum real hBar_eVs = 6.58211_899e-16;
+
+
+
+
+// ==================== MATHEMATICAL CONSTANTS ====================
+
+/// Euler-Mascheroni constant, &#x03B3;
 enum real eulerGamma = 0.57721_56649_01532_86060L;
 
