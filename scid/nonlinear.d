a95de3a37f784fdddeef5cb09985cffcbca5eeb6
Lars T. Kyllingstad <git@kyllingen.net>
2010-10-24 09:02:48 +0200

    Moved findNaN() from scid.util to scid.nonlinear

--- scid/nonlinear.d
+++ scid/nonlinear.d
@@ -482,3 +482,95 @@ unittest
     check(bracket.y1 * bracket.y2 <= 0);
     check(bracket.contains(0));
 }
+
+
+
+
+/** Use bisection to find the point where a function starts returning NaN.
+
+    Params:
+        f               =   The function.
+        xValid          =   A point where the function is known to
+                            return a valid result.
+        xNaN            =   A point where the function is known to
+                            return NaN.
+        fValid          =   (optional) The value of f at xValid.
+        xTolerance      =   Success: When the absolute distance between
+                            xValid and xNaN is less than this number,
+                            the function returns.
+        maxIterations   =   Failure: When the algorithm has failed to
+                            produce a result after maxIterations bisections,
+                            an exception is thrown.
+
+    Returns:
+    A tuple containing the values of xValid, xNaN, and fValid, where
+    xValid and xNaN now satisfy the success criterion
+    ---
+    abs(xValid-xNaN) <= xTolerance
+    ---
+
+    Example:
+    ---
+    auto r = findNaN(&log, 1.0, -1.0, 1e-6);
+    assert (r.xValid >= 0);
+    assert (r.xNaN < 0);
+    assert (abs(r.xValid - r.xNan) <= 1e-6);
+    ---
+*/
+Tuple!(T, "xValid", T, "xNaN", ReturnType!Func, "fValid") findNaN(Func, T)
+    (Func f, T xValid, T xNaN, T xTolerance, int maxIterations=40)
+in
+{
+    assert (isNaN(f(xNaN)), "f(xNaN) is not NaN");
+}
+body
+{
+    auto fValid = f(xValid);
+    return findNaN(f, xValid, xNaN, fValid, xTolerance, maxIterations);
+}
+
+
+/// ditto
+Tuple!(T, "xValid", T, "xNaN", R, "fValid") findNaN(Func, T, R)
+    (Func f, T xValid, T xNaN, R fValid, T xTolerance, int maxIterations=40)
+    if (isUnaryFunction!(Func, R, T) && isFloatingPoint!R)
+in
+{
+    assert (!isNaN(fValid), "fValid is NaN");
+    assert (isNaN(f(xNaN)), "f(xNaN) is not NaN");
+    assert (xTolerance > 0, "xTolerance must be positive");
+}
+body
+{
+    foreach (i; 0 .. maxIterations)
+    {
+        if (abs(xValid-xNaN) <= xTolerance)
+            return typeof(return)(xValid, xNaN, fValid);
+
+        immutable mid = (xValid + xNaN) / 2;
+        immutable fmid = f(mid);
+        if (isNaN(fmid))
+        {
+            xNaN = mid;
+        }
+        else
+        {
+            xValid = mid;
+            fValid = fmid;
+        }
+    }
+
+    enforceNE(false, NE.Limit);
+    assert(0);
+}
+
+    
+unittest
+{
+    auto r = findNaN(&log, 1.0, -1.0, 1e-6);
+    check (!isNaN(r.fValid));
+    check (log(r.xValid) == r.fValid);
+    check (r.xValid >= 0);
+    check (r.xNaN < 0);
+    check (abs(r.xValid-r.xNaN) <= 1e-6);
+}
