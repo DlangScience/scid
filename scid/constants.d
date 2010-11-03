e52f527ec3c646247ecb380ae9a76a3131897b2e
Lars T. Kyllingstad <git@kyllingen.net>
2010-11-03 14:43:34 +0100

    Improved scid.constants docs a bit

--- scid/constants.d
+++ scid/constants.d
@@ -11,16 +11,16 @@ module scid.constants;
 
 // ==================== PHYSICAL CONSTANTS ====================
 
-/// Planck constant, &#x210E; (J s)
+/// Planck constant, &#x210E; [J s]
 enum real planckConstant = 6.62606_896e-34L;
 
-/// Planck constant, &#x210E; (eV s)
+/// Planck constant, &#x210E; [eV s]
 enum real planckConstant_eVs = 4.13566_733e-15L;
 
-/// Planck constant divided by 2 pi, &#x210F; (J s)
+/// Reduced Planck constant, &#x210F; &#x2261; &#x210E;/2&#x03C0;  [J s]
 enum real hBar = 1.05457_1628e-34L;
 
-/// Planck constant divided by 2 pi, &#x210F; (eV s)
+/// Reduced Planck constant, &#x210F; &#x2261; &#x210E;/2&#x03C0;  [eV s]
 enum real hBar_eVs = 6.58211_899e-16;
 
 
