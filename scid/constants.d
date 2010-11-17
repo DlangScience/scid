ec239b1dee0bd2666d040da2e7c59af8b81a6b73
Lars T. Kyllingstad <git@kyllingen.net>
2010-11-17 13:05:09 +0100

    More accurate constants

--- scid/constants.d
+++ scid/constants.d
@@ -21,13 +21,19 @@ enum real planckConstant_eVs = 4.13566_733e-15L;
 enum real hBar = 1.05457_1628e-34L;
 
 /// Reduced Planck constant, &#x210F; &#x2261; &#x210E;/2&#x03C0;  [eV s]
-enum real hBar_eVs = 6.58211_899e-16;
+enum real hBar_eVs = 6.58211_899e-16L;
 
 
 
 
 // ==================== MATHEMATICAL CONSTANTS ====================
 
+/// Pi squared, &#x03C0;&#x00B2;
+enum real piSquared = 0x9.de9e64df22ef2d2p+0L;
+
+/// Two times pi, 2&#x03C0;
+enum real twoPi = 0xc.90fdaa22168c235p-1L;
+
 /// Euler-Mascheroni constant, &#x03B3;
-enum real eulerGamma = 0.57721_56649_01532_86060L;
+enum real eulerGamma = 0x9.3c467e37db0c7a5p-4L;
 
