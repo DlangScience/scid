8d81184943540546527ac65526f2341be93d4f1b
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2011-07-30 21:14:42 +0200

    Reintegrated David Simcha's fix to the RegionAllocator.

--- scid/internal/regionallocator.d
+++ scid/internal/regionallocator.d
@@ -80,7 +80,7 @@ Copyright:  Copyright (c) 2008-2011, David Simcha.
 License:    $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0)
 */
 
-module scid.internal.regionallocator;
+module std.regionallocator;
 
 import std.traits, core.memory, std.range, core.exception, std.conv,
     std.algorithm, std.typetuple, std.exception;
@@ -408,7 +408,8 @@ private:
 public:
 
     this(this) {
-        if(initialized) incrementRefCount();
+        ensureInitialized();
+        incrementRefCount();
     }
 
     ~this() {
@@ -786,7 +787,9 @@ unittest {
 }
 
 unittest {
-    RegionAllocator alloc;
+    auto alloc = newRegionAllocator();
+    auto alloc2 = alloc;
+
     double[] arr = alloc.uninitializedArray!(double[])(100);
     assert(arr.length == 100);
 
